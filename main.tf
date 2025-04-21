f6963e2e20265e1e5e65a40ebf30ad7363a2f136
Olakunle <mac@Olakunles-MacBook-Pro.local>
2025-04-21 05:12:43 +0100

    added new iac vm config

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
+terraform {
+    cloud {
+    organization = "my-iac-demo-org"
+    workspaces {
+      name = "iac-vm"
+    }
+    }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  features {}
+  # Configuration options
 }
 
 resource "azurerm_resource_group" "example" {
